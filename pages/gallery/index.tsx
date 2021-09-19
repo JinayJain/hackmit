e68ab6dd825c16c7e7cdaedddd790120ec46bc32
daniel-moon32 <dm32@illinois.edu>
2021-09-19 03:07:05 -0500

    add loading

--- pages/gallery/index.tsx
+++ pages/gallery/index.tsx
@@ -203,7 +203,7 @@ const Gallery: NextPage = () => {
         <Grid templateColumns="repeat(3, 1fr)" gap={4}>
           {/* {console.log(advancedFiltered)} */}
           {!error ? (
-            isLoaded && advancedFiltered.length > 0 ? (
+            isLoaded ? ( advancedFiltered.length > 0 ? (
               advancedFiltered.map((listing: Listing) => (
                 <LinkBox
                   key={listing._id}
@@ -232,6 +232,14 @@ const Gallery: NextPage = () => {
               <Box>No items founds</Box>
             )
           ) : (
+            <Spinner
+              thickness="4px"
+              speed="0.65s"
+              emptyColor="gray.200"
+              color="blue.500"
+              size="xl"
+            />
+          )): (
             <Box>Error, please try again</Box>
           )}
         </Grid>
