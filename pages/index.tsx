3f461d91c969c4527174ff5a11f1623f3a68ea4e
Jinay Jain <jinaybjain@gmail.com>
2021-09-19 05:25:28 -0400

    new home page w/o functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,51 +1,100 @@
-import type { GetServerSideProps, NextPage } from "next";
-import React from "react";
-import {
-    Box,
-    Button,
-    Heading,
-    Input,
-    InputGroup,
-    InputRightAddon,
-    InputRightElement,
-} from "@chakra-ui/react";
+import type { NextPage } from "next";
+import React, { useEffect, useState } from "react";
+import Autosuggest from "react-autosuggest";
+import { Box, Text, Heading, Flex, Button } from "@chakra-ui/react";
+import Fuse from "fuse.js";
+
+import theme from "../styles/suggest.module.css";
 
 const Home: NextPage = () => {
+    const [schools, setSchools] = useState<string[]>([]);
+    const [query, setQuery] = useState("");
+    const [suggestions, setSuggestions] = useState<string[]>([]);
+
+    const fuse = new Fuse(schools);
+
+    useEffect(() => {
+        async function getSchools() {
+            // get a list of the schools
+            const schools = await fetch("/api/schools").then((res) =>
+                res.json()
+            );
+
+            setSchools(schools);
+            console.log(schools);
+        }
+
+        getSchools();
+    }, []);
+
+    function renderSuggestion(suggestion: string) {
+        return (
+            <Box p={2}>
+                <Text>{suggestion}</Text>
+            </Box>
+        );
+    }
+
     return (
-        <Box bg="orange.500">
-            <section style={{ position: "relative", minHeight: "70vh" }}>
-                <Box pt="60px">
-                    <Heading fontSize="4rem" color="white" textAlign="center">
-                        Hand to Hand
+        <Box
+            bg="linear-gradient(15deg, rgba(150,62,90,1) 12%, rgba(218,143,17,1) 91%);"
+            h="100vh"
+        >
+            <section>
+                <Box pt="120px" textAlign="center">
+                    <Heading
+                        fontSize="4rem"
+                        color="white"
+                        textAlign="center"
+                        mb="6"
+                    >
+                        HandOff
+                    </Heading>
+                    <Heading fontWeight="light" color="white">
+                        Find new hands for your used college items
                     </Heading>
                     <Box maxW="1200px" margin="auto" mt={8}>
-                        <InputGroup>
-                            <Input
-                                fontSize="xl"
-                                placeholder="Find your college"
-                                type="search"
-                                bg="white"
+                        <Flex w="100%">
+                            <Autosuggest
+                                theme={theme}
+                                suggestions={suggestions}
+                                onSuggestionsFetchRequested={({ value }) => {
+                                    if (value.length > 15) {
+                                        setSuggestions(schools.slice(0, 5));
+                                    } else {
+                                        setSuggestions(
+                                            fuse
+                                                .search(value, {
+                                                    limit: 5,
+                                                })
+                                                .map((res) => res.item)
+                                        );
+                                    }
+                                }}
+                                onSuggestionsClearRequested={() =>
+                                    setSuggestions([])
+                                }
+                                getSuggestionValue={(s) => s}
+                                renderSuggestion={renderSuggestion}
+                                multiSection={false}
+                                inputProps={{
+                                    style: {
+                                        padding: "12px 24px",
+                                        borderRadius: "8px",
+                                        width: "100%",
+                                    },
+                                    onChange: (e, { newValue }) =>
+                                        setQuery(newValue),
+                                    value: query,
+                                    placeholder: "Search for a school",
+                                }}
                             />
-                            <InputRightAddon as={Button}>
-                                Search
-                            </InputRightAddon>
-                        </InputGroup>
+                        </Flex>
+                        <Button mt="4" colorScheme="orange">
+                            Begin your handoff
+                        </Button>
                     </Box>
                 </Box>
-
-                <div className="wave">
-                    <svg
-                        data-name="Layer 1"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 1200 120"
-                        preserveAspectRatio="none"
-                    >
-                        <path
-                            d="M600,112.77C268.63,112.77,0,65.52,0,7.23V120H1200V7.23C1200,65.52,931.37,112.77,600,112.77Z"
-                            className="shape-fill"
-                        ></path>
-                    </svg>
-                </div>
             </section>
         </Box>
     );
