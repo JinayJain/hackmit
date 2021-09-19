6c276af38330df2e1d07804f09cc79084c53e20b
Jinay Jain <jinaybjain@gmail.com>
2021-09-19 05:33:21 -0400

    home is /

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -260,7 +260,7 @@ const NAV_ITEMS: Array<NavItem> = [
             {
                 label: "Home",
                 subLabel: "",
-                href: "/home",
+                href: "/",
             },
             {
                 label: "Search Listings",
